1f1b701faaf6d6081f172ab68b52d8ad05a396e5
ebgaio <ebgaio@gmail.com>
2026-06-09 23:12:16 -0300

    - Melhorias no histórico, no uso dos moradores de um apartamento.

--- app/src/main/java/br/com/edificiopromenade/presentation/moradores/MoradoresScreen.kt
+++ app/src/main/java/br/com/edificiopromenade/presentation/moradores/MoradoresScreen.kt
@@ -1,14 +1,38 @@
 package br.com.edificiopromenade.presentation.moradores
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import br.com.edificiopromenade.presentation.util.formatarData
+import java.time.LocalDate
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -220,6 +244,9 @@ fun MoradoresScreen(
                         Column(
                             modifier = Modifier.padding(16.dp)
                         ) {
+                            var mostrarHistorico by remember {
+                                mutableStateOf(false)
+                            }
 
                             Text(
                                 text = "Apartamento ${item.apartamento.numero}"
@@ -240,7 +267,12 @@ fun MoradoresScreen(
 
                             val moradoresAtivos = item.moradores.filter { it.ativo }
 
-                            val moradoresHistorico = item.moradores.filter { !it.ativo }
+                            val moradoresHistorico =
+                                item.moradores
+                                    .filter { !it.ativo }
+                                    .sortedByDescending {
+                                        it.dataFim ?: LocalDate.MIN
+                                    }
 
                             moradoresAtivos.forEach { morador ->
 
@@ -251,11 +283,38 @@ fun MoradoresScreen(
                                         )
                                     }
                                 ) {
-                                    Text(morador.nome)
+                                    Text(
+                                        text = "🟢 ${morador.nome}"
+                                    )
                                 }
                             }
 
                             if (moradoresHistorico.isNotEmpty()) {
+
+                                Spacer(
+                                    modifier = Modifier.height(8.dp)
+                                )
+
+                                AssistChip(
+                                    onClick = {
+                                        mostrarHistorico = !mostrarHistorico
+                                    },
+                                    label = {
+
+                                        Text(
+                                            if (mostrarHistorico)
+                                                "Ocultar Histórico"
+                                            else
+                                                "Mostrar Histórico"
+                                        )
+                                    }
+                                )
+                            }
+
+                            if (
+                                mostrarHistorico &&
+                                moradoresHistorico.isNotEmpty()
+                            ) {
 
                                 Spacer(
                                     modifier = Modifier.height(8.dp)
@@ -268,8 +327,12 @@ fun MoradoresScreen(
                                 )
 
                                 Text(
-                                    text = "Histórico",
+                                    text = "Histórico de Moradores",
                                     style = MaterialTheme.typography.titleSmall
+                                )
+
+                                Spacer(
+                                    modifier = Modifier.height(4.dp)
                                 )
 
                                 moradoresHistorico.forEach { morador ->
@@ -279,25 +342,13 @@ fun MoradoresScreen(
                                     )
 
                                     Text(
-                                        text = morador.nome
+                                        text = "⚪ ${morador.nome}",
+//                                        style = MaterialTheme.typography.bodyMedium
                                     )
 
                                     Text(
-                                        text =
-                                            "Entrada: ${
-                                                formatarData(
-                                                    morador.dataInicio
-                                                )
-                                            }"
-                                    )
-
-                                    Text(
-                                        text =
-                                            "Saída: ${
-                                                formatarData(
-                                                    morador.dataFim
-                                                )
-                                            }"
+                                        text = "Encerrado em: ${ formatarData(morador.dataFim)}",
+                                        style = MaterialTheme.typography.bodySmall
                                     )
                                 }
                             }
